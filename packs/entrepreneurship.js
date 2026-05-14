a98c5be8aa9f22922e6379bca5061b3f5a1df62d
kaosphd <kaos.phd@gmail.com>
2026-05-14 15:13:28 +0100

    Create entrepreneurship.js

---
+++ packs/entrepreneurship.js
@@ -0,0 +1,36 @@
+window.PACKS = {
+
+  entrepreneurship: {
+
+    mechanics: [
+      "Drafting",
+      "Negotiation",
+      "Push-your-luck",
+      "Cooperative play",
+      "Resource management"
+    ],
+
+    settings: [
+      "Night market",
+      "Community workshop",
+      "Floating city",
+      "Startup incubator"
+    ],
+
+    tones: [
+      "Wholesome",
+      "Chaotic",
+      "Reflective",
+      "Competitive"
+    ],
+
+    wildcards: [
+      "Players cannot speak",
+      "Rules change halfway through",
+      "Everyone shares resources",
+      "One player knows hidden information"
+    ]
+
+  }
+
+};
